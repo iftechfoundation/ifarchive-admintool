c74c4d70ec8b1de4b6bed23348baeded59b9c82a
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-20 15:39:48 -0400

    Comments.

--- adminlib/info.py
+++ adminlib/info.py
@@ -1,6 +1,13 @@
 from adminlib.util import bad_filename, in_user_time
 
 class FileEntry:
+    """Represents one file in a directory.
+    
+    FileEntries are used in the templates which display lists of
+    files. Note that we don't cache these between requests; they
+    are created on the fly for each request.
+    """
+    
     # Some files have special meaning for the Archive and shouldn't be
     # moved or renamed.
     specialnames = set([
@@ -9,6 +16,9 @@ class FileEntry:
     ])
     
     def __init__(self, filename, stat, user=None):
+        # The user argument says what user to display this file *for*.
+        # (We use this to localize the time to their timezone.) If
+        # user is not provided, we'll display in UTC.
         self.name = filename
         self.date = stat.st_mtime
         self.size = stat.st_size
@@ -18,6 +28,14 @@ class FileEntry:
         self.fdate = mtime.strftime('%b %d, %H:%M %Z')
 
 class UploadEntry:
+    """Represents one entry in the upload log.
+    The arguments come straight from the "uploads" DB table.
+    
+    UploadEntries are used in the templates which display lists of
+    upload info. Note that we don't cache these between requests; they
+    are created on the fly for each request.
+    """
+    
     def __init__(self, args, user=None):
         (uploadtime, md5, size, filename, origfilename, donorname, donoremail, donorip, donoruseragent, permission, suggestdir, ifdbid, about) = args
         self.uploadtime = uploadtime

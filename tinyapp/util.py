1d9e325276776dc17b8c0aca684c927b5ee53d8c
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-20 15:03:37 -0400

    Comments.

--- tinyapp/util.py
+++ tinyapp/util.py
@@ -2,8 +2,13 @@ import os
 import time
 
 def random_bytes(count):
+    """Return N random bytes from a good source.
+    """
     byt = os.urandom(count)
     return bytes.hex(byt)
         
 def time_now():
+    """Return the current time. (Rounded to the nearest second
+    because that's good enough.)
+    """
     return int(time.time())

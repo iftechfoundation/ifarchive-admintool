4bbb5def258ce56902033d4bdd9b093b93b7c954
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-20 15:08:46 -0400

    Comments.

--- tinyapp/auth.py
+++ tinyapp/auth.py
@@ -2,16 +2,24 @@ from tinyapp.util import random_bytes
 from tinyapp.excepts import HTTPError
 
 def xsrf_cookie(req, han):
+    """Request filter which sets an XSRF cookie on any request. This cookie
+    can be used with web forms to prevent cross-site scripting attacks.
+    Add a hidden form field with name '_xsrf' and then check it in
+    the POST response using the filter below.
+    """
     if '_xsrf' in req.cookies:
         req._xsrf = req.cookies['_xsrf'].value
     else:
         req._xsrf = random_bytes(16)
+        ### Peeking at app.secure_site is a layer violation. Would be better to have separate xsrf_cookie(), xsrf_cookie_secure() wrappers.
         req.set_cookie('_xsrf', req._xsrf, httponly=True, secure=req.app.secure_site)
     return han(req)
 
-### make another with secure=True
 
 def xsrf_check_post(req, han):
+    """Request filter which checks the XSRF field of a POST response against
+    the user's XSRF cookie. (Has no effect on GET.)
+    """
     if req.request_method == 'POST':
         if ('_xsrf' not in req.input
             or not req.input['_xsrf']

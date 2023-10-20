723810195994091aea23214e8ef6f011f553e234
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-20 15:02:04 -0400

    Comments.

--- tinyapp/handler.py
+++ tinyapp/handler.py
@@ -1,6 +1,8 @@
 import re
 
 class ReqHandler:
+    """Base class for a URL handler.
+    """
     def __init__(self, app, pat):
         self.app = app
         
@@ -9,18 +11,32 @@ class ReqHandler:
         self.pat = re.compile(pat)
 
     def do_get(self, req):
+        """Handle GET.
+        """
         raise NotImplementedError('%s: GET not implemented' % (self.__class__.__name__,))
 
     def do_post(self, req):
+        """Handle POST.
+        """
         raise NotImplementedError('%s: POST not implemented' % (self.__class__.__name__,))
 
     def do_head(self, req):
+        """Handle HEAD.
+        This defaults to doing a GET but then responding with no data.
+        Not really ideal, since we wind up doing all the database work
+        (or whatever).
+        """
         for val in self.do_get(req):
             # Accumulate total size?
             pass
         yield ''
 
 class WrappedHandler:
+    """A class that wraps a ReqHandler, applying a request filter to all
+    GET and POST requests.
+    (We don't have to filter HEAD requests because those will invoke
+    GET anyway.)
+    """
     def __init__(self, han, wrapper):
         self.app = han.app
         self.pat = han.pat
@@ -30,6 +46,12 @@ class WrappedHandler:
         self.do_post = lambda req: wrapper(req, han.do_post)
 
 def before(wrapper):
+    """Handler decorator which applies a filter. Use within a Handler:
+
+      @before(wrapper)
+      def do_get(self, req):
+        ...
+    """
     def func(han):
         def subfunc(self, req):
             return wrapper(req, lambda req2: han(self, req2))
@@ -37,6 +59,13 @@ def before(wrapper):
     return func
 
 def beforeall(wrapper):
+    """Handler class decorator which applies a filter. Use on the entire
+    class:
+
+      @beforeall(wrapper)
+      class han_Foo(ReqHandler):
+        ...
+    """
     def func(cla):
         def subfunc(app, pat):
             inst = cla(app, pat)

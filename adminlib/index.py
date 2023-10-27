c5fbcc753d28ed800f732b67d421552c4b3f28c4
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-27 15:19:10 -0400

    Index parsing.

---
+++ adminlib/index.py
@@ -0,0 +1,99 @@
+import re
+from collections import OrderedDict
+import markdown
+
+filename_pattern = re.compile('^#[^#]')
+dashline_pattern = re.compile('^[ ]*[-+=#*]+[ -+=#*]*$')
+
+convertermeta = markdown.Markdown(extensions = ['meta'])
+
+class IndexFileEntry:
+    """One file entry in an Index file.
+    """
+    def __init__(self, filename):
+        self.submap = {}
+
+        self.name = filename
+        self.metadata = OrderedDict()
+
+    def __repr__(self):
+        return '<IndexFileEntry %s>' % (self.name,)
+
+    def complete(self, desclines):
+        # Take the accumulated description text and stick it into our
+        # File object.
+        if desclines:
+            val = '\n'.join(desclines)
+            filestr = convertermeta.convert(val)
+            for (mkey, mls) in convertermeta.Meta.items():
+                self.metadata[mkey] = list(mls)
+            convertermeta.Meta.clear()
+
+            ### stash filestr?
+
+    def getmetadata_string(self, key):
+        # Return a metadata value as a string. If there are multiple values,
+        # just use the first.
+        if key not in self.metadata or not self.metadata[key]:
+            return None
+        return self.metadata[key][0]
+
+
+def parse_index(pathname):
+    infl = open(pathname, encoding='utf-8')
+
+    files = []
+    
+    filedesclines = None
+    inheader = True
+    headerlines = []
+    file = None
+    
+    while True:
+        ln = infl.readline()
+        if not ln:
+            if file:
+                # Finish constructing the file in progress.
+                file.complete(filedesclines)
+                file = None
+            break
+        
+        ln = ln.rstrip()
+
+        bx = ln
+
+        if inheader:
+            if not filename_pattern.match(bx):
+                # Further header lines become part of headerlines.
+                headerlines.append(bx)
+                continue
+
+            # The header ends when we find a line starting with "#".
+            inheader = False
+            file = None
+
+        if filename_pattern.match(bx):
+            # Start of a new file block.
+
+            if file:
+                # Finish constructing the file in progress.
+                file.complete(filedesclines)
+                file = None
+
+            # Set up the new file, including a fresh filedesclines
+            # accumulator.
+
+            filename = bx[2:].strip()
+            bx = ''
+            filedesclines = []
+                
+            file = IndexFileEntry(filename)
+            files.append(file)
+
+        else:
+            # Continuing a file block.
+            filedesclines.append(bx)
+
+    infl.close()
+    
+    return files

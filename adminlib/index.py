54bc36a0dc55df46a3f564cd56f5fa8826deae01
Andrew Plotkin <zarf@ZarfLent.local>
2023-10-27 22:53:55 -0400

    Switch over to the old IndexDir/IndexFile code instead.

--- adminlib/index.py
+++ adminlib/index.py
@@ -1,121 +1,92 @@
 import re
+import os.path
 from collections import OrderedDict
-import markdown
 
+# A filename header starts with exactly one "#" (an h1 header in Markdown)
 filename_pattern = re.compile('^#[^#]')
-dashline_pattern = re.compile('^[ ]*[-+=#*]+[ -+=#*]*$')
 
-convertermeta = markdown.Markdown(extensions = ['meta'])
+# These patterns match the rules of metadata lines as defined in the
+# Markdown extension:
+# https://python-markdown.github.io/extensions/meta_data/
+meta_start_pattern = re.compile('^[ ]?[ ]?[ ]?([a-zA-Z0-9_-]+):')
+meta_cont_pattern = re.compile('^(    |\\t)')
 
-class IndexFile:
-    def __init__(self, headerlines, files):
-        self.files = files
-        self.filemap = { file.name:file for file in files }
-        self.metadata = OrderedDict()
+class IndexDir:
+    def __init__(self, dirname, rootdir=None):
+        self.dirname = dirname
+        self.indexpath = os.path.join(rootdir, dirname, 'Index')
 
-        headerstr = '\n'.join(headerlines)
-        headerstr = headerstr.rstrip() + '\n'
-        # Now headerstr starts with zero newlines and ends
-        # with one newline.
+        self.description = []
+        self.files = OrderedDict()
 
-        anyheader = bool(headerstr.strip())
-        if anyheader:
-            # Convert Markdown to HTML.
-            val = convertermeta.convert(headerstr)
-            for (mkey, mls) in convertermeta.Meta.items():
-                self.metadata[mkey] = list(mls)
-            convertermeta.Meta.clear()
-
-            ### stash val?
-            
-class IndexFileEntry:
-    """One file entry in an Index file.
-    """
-    def __init__(self, filename):
-        self.submap = {}
-
-        self.name = filename
-        self.metadata = OrderedDict()
-
-    def __repr__(self):
-        return '<IndexFileEntry %s>' % (self.name,)
-
-    def complete(self, desclines):
-        # Take the accumulated description text and stick it into our
-        # File object.
-        if desclines:
-            val = '\n'.join(desclines)
-            filestr = convertermeta.convert(val)
-            for (mkey, mls) in convertermeta.Meta.items():
-                self.metadata[mkey] = list(mls)
-            convertermeta.Meta.clear()
-
-            ### stash filestr?
-
-    def getmetadata_string(self, key):
-        # Return a metadata value as a string. If there are multiple values,
-        # just use the first.
-        if key not in self.metadata or not self.metadata[key]:
-            return None
-        return self.metadata[key][0]
-
-
-def parse_index(pathname):
-    infl = open(pathname, encoding='utf-8')
-
-    files = []
-    
-    filedesclines = None
-    inheader = True
-    headerlines = []
-    file = None
-    
-    while True:
-        ln = infl.readline()
-        if not ln:
-            if file:
-                # Finish constructing the file in progress.
-                file.complete(filedesclines)
-                file = None
-            break
+        # Parse the existing Index file.
+        infl = open(self.indexpath, encoding='utf-8')
+        curfile = None
+        curmetaline = None
         
-        ln = ln.rstrip()
-
-        bx = ln
-
-        if inheader:
-            if not filename_pattern.match(bx):
-                # Further header lines become part of headerlines.
-                headerlines.append(bx)
+        for ln in infl.readlines():
+            if filename_pattern.match(ln):
+                # File entry header.
+                filename = ln[1:].strip()
+                curfile = IndexFile(filename, self)
+                curmetaline = True
+                self.files[filename] = curfile
+                continue
+            
+            if not curfile:
+                # Directory description line.
+                self.description.append(ln)
                 continue
 
-            # The header ends when we find a line starting with "#".
-            inheader = False
-            file = None
+            # Part of the file entry.
+            if curmetaline is not None:
+                match = meta_start_pattern.match(ln)
+                match2 = meta_cont_pattern.match(ln)
+                if ln.strip() == '':
+                    curmetaline = None
+                elif match:
+                    # New metadata line
+                    curmetaline = match.group(1)
+                    val = ln[match.end() : ].strip()
+                    curfile.add_metadata(curmetaline, val, dirty=False)
+                    continue
+                elif type(curmetaline) is str and match2:
+                    val = ln[match2.end() : ].strip()
+                    curfile.add_metadata(curmetaline, val, dirty=False)
+                    continue
+                else:
+                    curmetaline = None
+                # We're done with the metadata, so this is a description line.
 
-        if filename_pattern.match(bx):
-            # Start of a new file block.
-
-            if file:
-                # Finish constructing the file in progress.
-                file.complete(filedesclines)
-                file = None
-
-            # Set up the new file, including a fresh filedesclines
-            # accumulator.
-
-            filename = bx[2:].strip()
-            bx = ''
-            filedesclines = []
+            # For consistency, the description will always start with a blank line.
+            if len(curfile.description) == 0 and ln.strip() != '':
+                curfile.description.append('\n')
                 
-            file = IndexFileEntry(filename)
-            files.append(file)
+            curfile.description.append(ln)
+            
+                
+        infl.close()
 
-        else:
-            # Continuing a file block.
-            filedesclines.append(bx)
+    def __repr__(self):
+        return '<IndexDir %s>' % (self.dirname,)
 
-    infl.close()
 
-    index = IndexFile(headerlines, files)
-    return index
+class IndexFile:
+    def __init__(self, filename, dir):
+        self.filename = filename
+        self.dir = dir
+        self.description = []
+        self.metadata = OrderedDict()
+        self.dirty = False
+        
+    def __repr__(self):
+        return '<IndexFile %s>' % (self.filename,)
+
+    def add_metadata(self, key, val, dirty=True):
+        ls = self.metadata.get(key)
+        if ls is None:
+            ls = []
+            self.metadata[key] = ls
+        ls.append(val)
+        if dirty:
+            self.dirty = True
